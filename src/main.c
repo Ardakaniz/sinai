8d5d21ea0447adbd2fb0a2b64625ea6a5a0495cd
Ardakaniz <ardakaniz@e.email>
2022-02-22 11:29:28 +0100

    Fix presence + add init conds control parameters

--- src/main.c
+++ src/main.c
@@ -2,12 +2,13 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "vec.h"
 
 #define ATOM_RADIUS   0.25 // As a proportion of lattice's size
-#define GRID_INTERVAL 0.005
-#define GRID_SIZE (size_t)((1.0 / GRID_INTERVAL) * (1.0 / GRID_INTERVAL))
+#define GRID_INTERVAL (1.0 / GRID_SIZE)
+#define GRID_SIZE 200
 
 /* STRUCTS */
 struct data_t {	
@@ -16,7 +17,7 @@ struct data_t {
 	vec_t* points; // Initial point + iterations
 	vec_t* directions; // Initial direction + iterations
 
-	unsigned int* presence;// [GRID_SIZE] ;
+	unsigned int* presence;// [GRID_SIZE^3] ;
 };
 typedef struct data_t data_t;
 
@@ -37,6 +38,8 @@ void iter(data_t* data);
 int main(int argc, char* argv[]) {
 	size_t ITER_COUNT = 50;
 	bool out_presence = false;
+	double x_i = 0., y_i = 0., dx_i = 1., dy_i = 1.;
+
 	for (int i = 1; i < argc; ++i) {
 		if (strcmp(argv[i], "-iter") == 0) {
 			if (argc < i + 1) {
@@ -44,23 +47,36 @@ int main(int argc, char* argv[]) {
 				return EXIT_FAILURE;
 			}
 
-			char* end;
-			ITER_COUNT = strtoul(argv[i + 1], &end, 10);
-			printf("ITER_COUNT = %llu\n", ITER_COUNT);
+			ITER_COUNT = strtoul(argv[i + 1], NULL, 10);
 
 			++i;
 		}
 		else if (strcmp(argv[i], "-presence") == 0) {
 			out_presence = true;
-			printf("OUT_DATA = presence\n");
+		}
+		else if (strcmp(argv[i], "-init") == 0) {
+			if (argc < i + 4) {
+				printf("-init argument expected initial position and direction\n");
+				return EXIT_FAILURE;
+			}
+
+			x_i  = strtod(argv[i + 1], NULL);
+			y_i  = strtod(argv[i + 2], NULL);
+			dx_i = strtod(argv[i + 3], NULL);
+			dy_i = strtod(argv[i + 4], NULL);
+
+			i += 4;
 		}
 		else {
 			printf("Unrecognized option '%s'\n", argv[i]);
 		}
 	}
-	
-	data_t data = { .iter_idx = 0 };
 
+	printf("ITER_COUNT = %llu\n", ITER_COUNT);
+	printf("OUT_DATA = %d\n", (int)out_presence);
+
+
+	data_t data = { .iter_idx = 0 };
 	data.points = malloc((ITER_COUNT + 1) * sizeof(vec_t));
 	if (!data.points) {
 		printf("Failed to allocate %llu bytes\n", (ITER_COUNT + 1) * sizeof(vec_t));
@@ -74,23 +90,24 @@ int main(int argc, char* argv[]) {
 		return EXIT_FAILURE;
 	}
 
-	data.presence = malloc(GRID_SIZE * sizeof(unsigned int));
+	/* Initial pos */
+	data.points[0].x = x_i;
+	data.points[0].y = y_i;
+	/* Initial direction */
+	data.directions[0].x = dx_i;
+	data.directions[0].y = dy_i;
+	vec_normalize(&data.directions[0]);
+	printf("(x_i, y_i) = (%f, %f)\n(dx_i, dy_i) = (%f, %f)\n", x_i, y_i, data.directions[0].x, data.directions[0].y);
+
+	data.presence = malloc(GRID_SIZE * GRID_SIZE * sizeof(unsigned int));
 	if (!data.directions) {
-		printf("Failed to allocate %llu bytes\n", GRID_SIZE * sizeof(unsigned int));
+		printf("Failed to allocate %llu bytes\n", GRID_SIZE * GRID_SIZE * sizeof(unsigned int));
 		free(data.points);
 		free(data.directions);
 		return EXIT_FAILURE;
 	}
-	for (size_t i = 0; i < GRID_SIZE; ++i)
+	for (size_t i = 0; i < GRID_SIZE * GRID_SIZE; ++i)
 		data.presence[i] = 0;
-	
-	/* Initial pos */
-	data.points[0].x = 0;
-	data.points[0].y = 0;
-	/* Initial direction */
-	data.directions[0].x = 0;
-	data.directions[0].y = 1;
-	vec_normalize(&data.directions[0]);
 
 	for (size_t i = 0; i < ITER_COUNT; ++i)
 		iter(&data);
@@ -106,7 +123,7 @@ int main(int argc, char* argv[]) {
 	}
 
 	if (out_presence) {
-		for (size_t i = 0; i < GRID_SIZE; ++i) {
+		for (size_t i = 0; i < GRID_SIZE * GRID_SIZE; ++i) {
 			fprintf(file, "%u\n", data.presence[i]);
 		}
 	}
@@ -126,12 +143,10 @@ int main(int argc, char* argv[]) {
 /* FUNCTIONS IMPL */
 
 size_t coords2idx(const vec_t* coords) {
-	const size_t x_idx = (size_t)floor((coords->x + 0.5) / GRID_INTERVAL);
-	const size_t y_idx = (size_t)floor((coords->y + 0.5) / GRID_INTERVAL);
+	const size_t x_idx = (size_t)floor(fmax(0.0, fmin(1.0, coords->x + 0.5)) * (GRID_SIZE - 1.0));
+	const size_t y_idx = (size_t)floor(fmax(0.0, fmin(1.0, coords->y + 0.5)) * (GRID_SIZE - 1.0));
 
-	const size_t grid_width  = (size_t)(1.0 / GRID_INTERVAL); // Grid of width 1 with intervals of 0.01
-
-	return y_idx * grid_width + x_idx;
+	return y_idx * GRID_SIZE + x_idx;
 }
 
 int intersect_atom(vec_t* point, vec_t* dir, const vec_t* atom_pos, intersection_t* intersection) {
@@ -187,8 +202,8 @@ void iter(data_t* data) {
 		const size_t coords = coords2idx(&presence_point);
 		data->presence[coords]++;
 
-		presence_point.x += GRID_INTERVAL * data->directions[data->iter_idx].x;
-		presence_point.y += GRID_INTERVAL * data->directions[data->iter_idx].y;
+		presence_point.x += data->directions[data->iter_idx].x / GRID_SIZE;
+		presence_point.y += data->directions[data->iter_idx].y / GRID_SIZE;
 	}
 
 	++data->iter_idx;
